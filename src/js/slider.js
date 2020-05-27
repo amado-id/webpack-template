eba537be6a83094e00c91d42993e149afaaf12f6
Fanshys <dmitriy.shab.mail@gmail.com>
2020-05-27 22:33:21 +0500

    first commit

---
+++ src/js/slider.js
@@ -0,0 +1,6 @@
+import Swiper from 'swiper'
+
+const slider = new Swiper('.swiper-container', {
+	spaceBetween: 20,
+	slidesPerView: 3
+})
---
+++ webpack.config.js
@@ -0,0 +1,105 @@
+const path = require('path');
+const fs = require('fs');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const autoprefixer = require('autoprefixer');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+function generateHtmlPlugins(templateDir) {
+	const templateFiles = fs.readdirSync(path.resolve(__dirname, templateDir))
+	return templateFiles.map(item => {
+		const parts = item.split('.')
+		const name = parts[0]
+		const extension = parts[1]
+		return new HtmlWebpackPlugin({
+			filename: `${name}.html`,
+			template: path.resolve(__dirname, `${templateDir}/${name}.${extension}`),
+			minify: false
+		})
+	})
+}
+
+const htmlPlugins = generateHtmlPlugins('./src/pug/pages')
+
+module.exports = {
+	context: path.resolve(__dirname, 'src'),
+	entry: './js/index.js',
+	output: {
+		filename: 'js/bundle.js',
+		path: path.resolve(__dirname, 'dist')
+	},
+	devtool: "source-map",
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				include: path.resolve(__dirname, 'src/js'),
+				use: {
+					loader: 'babel-loader',
+					options: {
+						presets: ['@babel/preset-env']
+					}
+				}
+			},
+			{
+				test: /\.css$/i,
+				use: ['style-loader', 'css-loader'],
+			},
+			{
+				test: /\.(sa|sc|c)ss$/i,
+				use: [
+					{
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              publicPath: '../',
+            },
+          },
+					'css-loader',
+					{
+						loader: 'postcss-loader',
+						options: {
+							plugins: [
+								autoprefixer()
+							],
+							sourceMap: true
+						}
+					},
+					'sass-loader',
+				],
+			},
+			{
+				test: /\.(png|jpe?g|gif|svg)$/i,
+				loader: 'file-loader',
+				options: {
+					name: '[name].[ext]',
+					outputPath: 'images'
+				}
+			},
+			{
+				test: /\.(ttf|woff|woff2|eot|otf)$/i,
+				loader: 'file-loader',
+				options: {
+					name: '[name].[ext]',
+					outputPath: 'fonts'
+				}
+			},
+			{
+				test: /\.pug$/,
+				loader: 'pug-loader',
+				query: { pretty: true }
+			}
+		]
+	},
+	plugins: [
+		new CopyPlugin({
+			patterns: [
+				{ from: 'images', to: 'images' },
+			],
+		}),
+		new MiniCssExtractPlugin({
+			filename: 'css/style.min.css'
+		}),
+		new CleanWebpackPlugin(),
+	].concat(htmlPlugins)
+}
